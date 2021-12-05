082a6a40e0cff341159c9279b45a3836c31d1277
SpencerLaudie <rsl34@byu.edu>
2021-12-05 14:53:50 -0700

    added create event page

--- front-end/src/views/CreateEvent.vue
+++ front-end/src/views/CreateEvent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="create-event-container">
-    <div class="create-event">
+    <div class="create-event-box">
       <form class="" @submit.prevent="create">
         <legend>Create an Event</legend>
         <input v-model='title' placeholder="title">
@@ -40,4 +40,59 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.create-event-container {
+  flex-grow: 1;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.create-event-box {
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  padding: 50px 100px;
+  box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+  border-radius: 30px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 14px;
+
+}
+
+form legend {
+  font-weight: 700;
+  font-size: 25px;
+}
+
+input {
+  margin-top: 10px;
+  border: none;
+  border-bottom: 2px solid #B5B5B5;
+  padding: 8px 0;
+}
+
+input:focus {
+  outline: none;
+}
+
+.button {
+  margin-top: 20px;
+  width: fit-content;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 20px;
+  font-weight: bold;
+  color: white;
+  background-color: #f6a192;
+}
+
+.button:hover {
+  background-color: #f6b092;
+}
+
 </style>
