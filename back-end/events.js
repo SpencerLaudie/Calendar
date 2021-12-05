d29db4ba400d28b6323c4c7716d7a59ab9a4e95d
SpencerLaudie <rsl34@byu.edu>
2021-12-05 14:19:55 -0700

    initial commit

---
+++ back-end/events.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const router = express.Router();
+
+const users = require("./users.js");
+const User = users.model;
+const validUser = users.valid;
+
+const eventSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  },
+  title: String,
+  date: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+const Event = mongoose.model('Event', eventSchema);
+
+router.post("/", validUser, async (req, res) => {
+  const event = new Event({
+    user: req.user,
+    title: req.body.title,
+    date: req.body.date
+  });
+  try {
+    await event.save();
+    return res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+});
+
+router.get("/", validUser, async (req, res) => {
+  try {
+    let events = await Event.find({
+      user: req.user
+    }).populate('user');
+    return res.send(events);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+});
+
+module.exports = {
+  model: Event,
+  routes: router,
+}
