f6b56d99afe5a43b12c8c485f6cd98a87af7ebba
SpencerLaudie <rsl34@byu.edu>
2021-12-05 21:57:32 -0700

    populate events

--- front-end/src/components/Calendar.vue
+++ front-end/src/components/Calendar.vue
@@ -20,7 +20,7 @@
       <div id="calendar-days">
         <div class='calendar-day' v-for='day in days'  v-bind:class="{'off-month': !day.isCurrentMonth, 'event': events.length > 0}" v-bind:key='day.date.toString()'>
           <div class="dayNum">{{day.dayOfMonth}}</div>
-          <div v-for='event in events' v-bind:key="event.date.toString()">{{event.title}}</div>
+          <div v-for='event in dailyEvents(day.date)' v-bind:key="event.date.toString()">{{event.title}}</div>
         </div>
       </div>
     </div>
@@ -48,8 +48,8 @@ export default {
       var nextMonthDays = this.createNextMonth(this.year, this.month);
       return [...previousMonthDays, ...currentMonthDays, ...nextMonthDays];
     },
-    monthlyEvents: function() {
-      return this.events.filter(event => (event.date.getFullYear == this.year && event.date.getMonth == this.month));
+    dailyEvents: function(day) {
+      return this.events.filter(event => (event.date.getFullYear == this.year && event.date.getMonth == this.month && event.date.getDate == day));
     },
     monthString: function() {
       switch (this.month) {
